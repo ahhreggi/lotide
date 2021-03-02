eb7842afd64788f20f71f290f442690c10894c2c
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-02 17:45:02 +0000

    Add without.js

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  const filteredArray = [];
+  for (let item of source) {
+    if (!itemsToRemove.includes(item)) {
+      filteredArray.push(item);
+    }
+  }
+  return filteredArray;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
