78c729d24e4a1957c6f131669452caebcb28cf99
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-02 18:05:11 +0000

    Add flatten.js

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+const flatten = function(array) {
+  const flattenedArray = [];
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      for (let innerItem of item) {
+        flattenedArray.push(innerItem);
+      }
+    } else {
+      flattenedArray.push(item);
+    }
+  }
+  return flattenedArray;
+};
+
+// Test Cases
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([[1, 2, 3, 4, 5, 6]]), [1, 2, 3, 4, 5, 6]);
