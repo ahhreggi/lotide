101df6ab61a11d819a81f6b4d1c1c33dcfdec8cd
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-03 20:40:42 +0000

    Fix linting, add eqArrays to assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,8 +1,20 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
   // If the objects don't have the same length, they are not equal
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
+  } else {  
     // For each key in object1
     for (const key of Object.keys(object1)) {
       // If the key's value in both objects are arrays, check if they are equal
@@ -26,8 +38,11 @@ const assertObjectsEqual = function(actual, expected) {
   } else {
     console.log("🛑🛑🛑 Assertion Failed:", inspect(actual), "!==", inspect(expected));
   }
-}
+};
 
 // Test Cases
 assertObjectsEqual({a: 1, b: 2}, {b: 2, a: 1});
 assertObjectsEqual({c: "foo", d: "bar"}, {d: "bar", c: "foo"});
+assertObjectsEqual({a: [1, 2]}, {a: [1, 2]});
+assertObjectsEqual({a: [1, 2]}, {a: [2, 3]});
+assertObjectsEqual({a: [1, 2]}, {a: "not an array"});
