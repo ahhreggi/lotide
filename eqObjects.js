ae90d0f8aee9ec7b508a99a40e9b04351d111104
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-03 20:25:40 +0000

    Add eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,59 @@
+const assertEqual = function(actual, expected) {
+  let strAct = typeof actual === "string" ? '"' : '';
+  let strExp = typeof expected === "string" ? '"' : '';
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${strAct}${actual}${strAct} === ${strExp}${expected}${strExp}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${strAct}${actual}${strAct} !== ${strExp}${expected}${strExp}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Return true if both objects have identical keys with identical values
+// Otherwise, return false
+const eqObjects = function(object1, object2) {
+  // If the objects don't have the same length, they are not equal
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    // For each key in object1
+    for (const key of Object.keys(object1)) {
+      // If the key's value in both objects are arrays, check if they are equal
+      // Otherwise, compare the primitive values normally
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// Test Cases
+const ab = {a: "1", b: "2"};
+const ba = {b: "2", a: "1"};
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
