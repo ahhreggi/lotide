d58ede688a9f22aa30d4ca23c9b11cc3423329f3
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-05 19:57:21 +0000

    Fix linting

--- findKey.js
+++ findKey.js
@@ -10,10 +10,10 @@ const assertEqual = function(actual, expected) {
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
-      return key
+      return key;
     }
   }
-}
+};
 
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
