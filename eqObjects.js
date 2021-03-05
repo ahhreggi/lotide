d8287e9c2c3b741d3b359e7e05ee77f2d9c1fc52
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-05 20:14:32 +0000

    Refactor eqObjects to support nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -30,11 +30,14 @@ const eqObjects = function(object1, object2) {
     // For each key in object1
     for (const key of Object.keys(object1)) {
       // If the key's value in both objects are arrays, check if they are equal
-      // Otherwise, compare the primitive values normally
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
+      // If the key's value in both objects are objects, check if they are equal
+      } else if (typeof object1[key] === "object" && typeof object2[key] === "object") {
+        return eqObjects(object1[key], object2[key]);
+      // Otherwise, compare the primitive values normally
       } else if (object1[key] !== object2[key]) {
         return false;
       }
@@ -44,6 +47,9 @@ const eqObjects = function(object1, object2) {
 };
 
 // Test Cases
+
+console.log('--- Standard Test Cases ---');
+
 const ab = {a: "1", b: "2"};
 const ba = {b: "2", a: "1"};
 assertEqual(eqObjects(ab, ba), true);
@@ -57,3 +63,13 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+console.log('--- Recursive Test Cases ---');
+
+const x = { a: {c: 1}, b: {d: 2}};
+const y = { b: {d: 2}, a: {c: 1}};
+assertEqual(eqObjects(x, y), true);
+
+const x1 = { a: {c: {i: 1}}, b: {d: {j: 2}}};
+const y1 = { a: {c: {i: 1}}, b: {d: {j: 2}}};
+assertEqual(eqObjects(x1, y1), true);
