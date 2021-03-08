8363a6b24eed8751cbddd94c23694fd3f6df526b
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-08 18:13:32 +0000

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
-  }
-};
-
 const middle = function(array) {
   const result = [];
   // If the array length is odd, length % 2 is not 0, so return the middle element
@@ -31,7 +11,4 @@ const middle = function(array) {
   return result;
 };
 
-// Test Cases
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
