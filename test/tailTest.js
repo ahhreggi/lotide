1050bb25f76b0203863e9cd5d0f001e4551c87df
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-08 18:39:06 +0000

    Add index.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(String(result), String(["Lighthouse", "Labs"]));
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case: An array with only one element should yield an empty array for its tail
-const shortArray = tail(["alone"]);
-assertEqual(shortArray.length, 0);
-
-// Test Case: An empty array should yield an empty array for its tail
-const emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("should return an empty array for an array with one element", () => {
+    assert.deepEqual(tail(["Yo Yo"]), []);
+  });
+  it("should return an ampty array for an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
